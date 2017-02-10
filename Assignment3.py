eab4da4fa000a84eeec8f014321f5d808f06b155
Dakota <dakotab@bgsu.edu>
2017-02-10 18:23:02 -0500

    Large update
    Added more buttons, text input, file input, etc.

--- Assignment3.py
+++ Assignment3.py
@@ -1,4 +1,4 @@
-import pygame, sys, os, time, math, re, eztext
+import pygame, sys, os, time, math, re, pygame_textinput
 from pygame.locals import *
 from sys import exit
 from random import randint
@@ -14,7 +14,7 @@ background = back.convert()
 pygame.display.set_caption("Assignment #3")
 
 #font objects
-smallText = pygame.font.Font("font/gameplay.ttf", 25)
+smallText = pygame.font.Font("font/gameplay.ttf", 12)
 
 #program mechanics
 introBG = pygame.image.load("img/introBG.png")
@@ -27,7 +27,30 @@ rulesButton = pygame.image.load("img/rulesButton.png")
 lightRulesButton = pygame.image.load("img/lightRulesButton.png")
 exitButton = pygame.image.load("img/exitButton.png")
 lightExitButton = pygame.image.load("img/lightExitButton.png")
+nrzlButton = pygame.image.load("img/nrzlButton.png")
+lightNrzlButton = pygame.image.load("img/lightNrzlButton.png")
+nrziButton = pygame.image.load("img/nrziButton.png")
+lightNrziButton = pygame.image.load("img/lightNrziButton.png")
+bamiButton = pygame.image.load("img/bamiButton.png")
+lightBamiButton = pygame.image.load("img/lightBamiButton.png")
+pdtyButton = pygame.image.load("img/pdtyButton.png")
+lightPdtyButton = pygame.image.load("img/lightPdtyButton.png")
+mchrButton = pygame.image.load("img/mchrButton.png")
+lightMchrButton = pygame.image.load("img/lightMchrButton.png")
+dmhrButton = pygame.image.load("img/dmhrButton.png")
+lightDmhrButton = pygame.image.load("img/lightDmhrButton.png")
+b8zsButton = pygame.image.load("img/b8zsButton.png")
+lightB8zsButton = pygame.image.load("img/lightB8zsButton.png")
+hdb3Button = pygame.image.load("img/hdb3Button.png")
+lightHdb3Button = pygame.image.load("img/lightHdb3Button.png")
+enterButton = pygame.image.load("img/enterButton.png")
+lightEnterButton = pygame.image.load("img/lightEnterButton.png")
+fileButton = pygame.image.load("img/fileButton.png")
+lightFileButton = pygame.image.load("img/lightFileButton.png")
+
+
 txtbx = "01001100011"
+txtbx2 = ""
 asciiArray = []
 fileArray = []
 
@@ -42,10 +65,14 @@ running = True
 intro = True
 results = True
 instruct = True
-    
+
+def Change():
+    global txtbx, txtbx2
+    txtbx = str(txtbx2.get_text())
+
 #re needs to be fixed
-def FileIO(fileName):
-    file = open(fileName)
+def FileIO():
+    file = open("file.txt")
     contents = file.readlines()
     file.close()
     for content in contents:
@@ -132,8 +159,13 @@ def NRZL():
             asciiArray[i] = "x"
 
     txtSurf, txtRect = text_objects(' | '.join(asciiArray), smallText, (0,0,255))
-    txtRect.center = ((400),(360))
+    txtRect.center = ((400),(400))
     screen.blit(txtSurf, txtRect)
+
+    nameSurf, nameRect = text_objects('Nonreturn to Zero-Level (NRZ-L)', smallText, (0,0,255))
+    nameRect.center = ((400),(380))
+    screen.blit(nameSurf, nameRect)
+
     
                 
 def NRZI():
@@ -184,8 +216,13 @@ def NRZI():
                 asciiArray[x] = "x"
 
     txtSurf, txtRect = text_objects(' | '.join(asciiArray), smallText, (0,0,255))
-    txtRect.center = ((400),(360))
+    txtRect.center = ((400),(400))
     screen.blit(txtSurf, txtRect)
+
+    nameSurf, nameRect = text_objects('Nonreturn to Zero Inverted (NRZI)', smallText, (0,0,255))
+    nameRect.center = ((400),(380))
+    screen.blit(nameSurf, nameRect)
+
 
 def BAMI():
     global txtbx, asciiArray
@@ -220,8 +257,13 @@ def BAMI():
                 asciiArray[x] = "x"
 
     txtSurf, txtRect = text_objects(' | '.join(asciiArray), smallText, (0,0,255))
-    txtRect.center = ((400),(360))
+    txtRect.center = ((400),(400))
     screen.blit(txtSurf, txtRect)
+
+    nameSurf, nameRect = text_objects('Bipolar-AMI', smallText, (0,0,255))
+    nameRect.center = ((400),(380))
+    screen.blit(nameSurf, nameRect)
+
     
 def PDTY():
     global txtbx, asciiArray
@@ -256,8 +298,12 @@ def PDTY():
                 asciiArray[x] = "x"
 
     txtSurf, txtRect = text_objects(' | '.join(asciiArray), smallText, (0,0,255))
-    txtRect.center = ((400),(360))
+    txtRect.center = ((400),(400))
     screen.blit(txtSurf, txtRect)
+
+    nameSurf, nameRect = text_objects('Pseudoternary', smallText, (0,0,255))
+    nameRect.center = ((400),(380))
+    screen.blit(nameSurf, nameRect)
     
 def MCHR():
     global txtbx, asciiArray
@@ -273,8 +319,12 @@ def MCHR():
             asciiArray[x] = "x"
 
     txtSurf, txtRect = text_objects(' | '.join(asciiArray), smallText, (0,0,255))
-    txtRect.center = ((400),(360))
+    txtRect.center = ((400),(400))
     screen.blit(txtSurf, txtRect)
+
+    nameSurf, nameRect = text_objects('Manchester', smallText, (0,0,255))
+    nameRect.center = ((400),(380))
+    screen.blit(nameSurf, nameRect)
     
 def DMHR():
     global txtbx, asciiArray
@@ -318,12 +368,92 @@ def DMHR():
 
 
     txtSurf, txtRect = text_objects(' | '.join(asciiArray), smallText, (0,0,255))
-    txtRect.center = ((400),(360))
+    txtRect.center = ((400),(400))
     screen.blit(txtSurf, txtRect)
+
+    nameSurf, nameRect = text_objects('Differential Manchester', smallText, (0,0,255))
+    nameRect.center = ((400),(380))
+    screen.blit(nameSurf, nameRect)
     
-#def B8ZS():
-#def HDB3():
-                
+def B8ZS():
+    global txtbx, asciiArray
+    txtarr = list(txtbx)
+    asciiArray = list(txtbx)
+    last = -1
+
+    for x, (i, j) in enumerate(zip(txtarr, asciiArray)):
+        if x > 0:
+            if txtarr[x] == "0":
+                asciiArray[x] = "_"
+            elif txtarr[x] == "1" and last == -1:
+                asciiArray[x] = "+"
+                last = x
+            elif txtarr[x] == "1" and last > -1:
+                if asciiArray[last] == "+":
+                    asciiArray[x] = "-"
+                elif asciiArray[last] == "-":
+                    asciiArray[x] = "+"
+                else:
+                    asciiArray[x] = "x"
+                last = x
+            else:
+                asciiArray[x] = "x"
+        else:
+            if txtarr[x] == "0":
+                asciiArray[x] = "_"
+            elif txtarr[x] == "1":
+                asciiArray[x] = "+"
+                last = x
+            else:
+                asciiArray[x] = "x"
+
+    txtSurf, txtRect = text_objects(' | '.join(asciiArray), smallText, (0,0,255))
+    txtRect.center = ((400),(400))
+    screen.blit(txtSurf, txtRect)
+
+    nameSurf, nameRect = text_objects('B8ZS', smallText, (0,0,255))
+    nameRect.center = ((400),(380))
+    screen.blit(nameSurf, nameRect)
+    
+def HDB3():
+    global txtbx, asciiArray
+    txtarr = list(txtbx)
+    asciiArray = list(txtbx)
+    last = -1
+
+    for x, (i, j) in enumerate(zip(txtarr, asciiArray)):
+        if x > 0:
+            if txtarr[x] == "0":
+                asciiArray[x] = "_"
+            elif txtarr[x] == "1" and last == -1:
+                asciiArray[x] = "+"
+                last = x
+            elif txtarr[x] == "1" and last > -1:
+                if asciiArray[last] == "+":
+                    asciiArray[x] = "-"
+                elif asciiArray[last] == "-":
+                    asciiArray[x] = "+"
+                else:
+                    asciiArray[x] = "x"
+                last = x
+            else:
+                asciiArray[x] = "x"
+        else:
+            if txtarr[x] == "0":
+                asciiArray[x] = "_"
+            elif txtarr[x] == "1":
+                asciiArray[x] = "+"
+                last = x
+            else:
+                asciiArray[x] = "x"
+
+    txtSurf, txtRect = text_objects(' | '.join(asciiArray), smallText, (0,0,255))
+    txtRect.center = ((400),(400))
+    screen.blit(txtSurf, txtRect)
+
+    nameSurf, nameRect = text_objects('HDB3', smallText, (0,0,255))
+    nameRect.center = ((400),(380))
+    screen.blit(nameSurf, nameRect)
     
 #Used for displaying text on screen
 def text_objects(text, font, color):
@@ -381,8 +511,9 @@ def Instructions():
 
 #loop that runs the the program
 def ProgramLoop():
-    global running, intro, results, instruct, xPos
+    global running, intro, results, instruct, xPos, txtbx, txtbx2
     white = pygame.Color(255, 255, 255, 255)
+    txtbx = "01001100011"
     running = True
     intro = False
     results = False
@@ -393,44 +524,66 @@ def ProgramLoop():
     canvas_height = 480
     centerY = int(canvas_height/2)
     blue = (0,0,255) # color of the wave
-    FPS = 160
     xPos = 0
     posRecord = {'square': []}
-    button1 = Button(mainButton, lightMainButton, 50, 400, ProgramIntro)
-    button2 = Button(rulesButton, lightRulesButton, 350, 400, Instructions)
-    button3 = Button(exitButton, lightExitButton, 650, 400, Exit)
-    allSprites = pygame.sprite.Group()
-    allSprites.add(button1, button2, button3)
+    button1 = Button(mainButton, lightMainButton, 400, 425, ProgramIntro)
+    button2 = Button(rulesButton, lightRulesButton, 525, 425, Instructions)
+    button3 = Button(exitButton, lightExitButton, 650, 425, Exit)
 
-#        deltay=25
-#        ypos = 275
-#        xpos = 50
-#        txtbx = eztext.Input(maxlength=7, color=(79,44,29), x=xpos, y=ypos, prompt="Enter Binary: ")
-#        ypos+=deltay
-#
-#        foci=0 #The focus index
-#        txtbx.focus=True
-#        txtbx.color=(79,44,29)
+    button4 = Button(nrzlButton, lightNrzlButton, 0, 10, NRZL)
+    button5 = Button(nrziButton, lightNrziButton, 100, 10, NRZI)
+    button6 = Button(bamiButton, lightBamiButton, 200, 10, BAMI)
+    button7 = Button(pdtyButton, lightPdtyButton, 300, 10, PDTY)
+    button8 = Button(mchrButton, lightMchrButton, 400, 10, MCHR)
+    button9 = Button(dmhrButton, lightDmhrButton, 500, 10, DMHR)
+    button10 = Button(b8zsButton, lightB8zsButton, 600, 10, B8ZS)
+    button11 = Button(hdb3Button, lightHdb3Button, 700, 10, HDB3)
+    
+    button12 = Button(enterButton, lightEnterButton, 600, 75, Change)
+    button13 = Button(fileButton, lightFileButton, 100, 425, FileIO)
+
+
+    allSprites = pygame.sprite.Group()
+    allSprites.add(button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13)
+
+    txtbx2 = pygame_textinput.TextInput()
 
     #program loop
     while running:
         Quit()
 
-        #display of frames
-        clock.tick(FPS)
-        text = "FPS: {0:.2f}".format(clock.get_fps())
-        pygame.display.set_caption(text)
-
         screen.fill(white)
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+
+        # Feed it with events every frame
+        txtbx2.update(events)
+        # Blit its surface onto the screen
+        screen.blit(txtbx2.get_surface(), (10, 75))
         
         button1.update()
         button2.update()
         button3.update()
+        button4.update()
+        button5.update()
+        button6.update()
+        button7.update()
+        button8.update()
+        button9.update()
+        button10.update()
+        button11.update()
+        button12.update()
+        button13.update()
         allSprites.draw(screen)
-        randomWaves(canvas_width, canvas_height, centerY, blue, posRecord)
-        DMHR()
         
         pygame.display.update()
+        clock.tick(60)
+        text = "FPS: {0:.2f}".format(clock.get_fps())
+        pygame.display.set_caption(text)
+
 
     
 def main():
--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Hello Dr. Green. I hope you're having a lovely day. This is the README file for 
 
 The languages needed to run this project are: Either Python 2.7.1 or Python 3.5 and Pygame. The links to download both have been added below.
 
-Description: I will be attempting to use python, and hopefully pygame (extra credit points?!) to create a GUI based program on a 30fps clock with I/O to graphically show the different encoding properties. Output of the clock speeds will be displayed in realtime on the window title. I will be using photoshop as well to create (hopefully) a nice background and buttons to switch between screens.
+Description: I used python and pygame (extra credit points?!) to create a GUI based program on a 30fps clock with I/O to graphically show the different encoding properties. To see a display of the output, press and hold one of the buttons, and it will be displayed near the bottom of the screen. Output of the clock speed is displayed in realtime as the window title. I used photoshop as well to create backgrounds and buttons to switch between the screens. For a file to be read, change the file name to "file.txt".
 
 Python: https://www.python.org/
 Pygame: http://www.pygame.org/lofi.html
